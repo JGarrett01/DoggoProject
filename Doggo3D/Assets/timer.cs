4ec10984724ce9414384d3609cc4411852c51bc4
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-08-16 15:33:40 +1000

    All dogs done

---
+++ Doggo3D/Assets/timer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class timer : MonoBehaviour {
+
+    public float time = 30;
+    public float starttimer = 2;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+        
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = time - Time.deltaTime;
+        starttimer = starttimer -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+
+            Fungus.Flowchart.BroadcastFungusMessage("FetchCall");
+            time = 30;
+
+
+        }
+
+        
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "TennisBall" && starttimer <= 0)
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Fetched");
+            
+
+        }
+
+    }
+}
